637f802cdd00b5cff64237ac5df380e668439c68
andydick1947 <ardick2000@hotmail.com>
2024-09-09 07:12:48 +0100

    Revamp of site

---
+++ res-bar.js
@@ -0,0 +1,21 @@
+let myRes = ["0","0"]
+
+let myResults = [
+["8 APR 24","Ranks 4 8 24.htm"],
+["15 APR 24","Ranks 4 15 24.htm"],
+["22 APR 24","Ranks 4 22 24.htm"],
+["29 APR 24","Ranks 4 29 24.htm"],
+["20 MAY 24","Ranks 5 20 24.htm"],
+["17 JUN 24","Ranks 6 17 24.htm"],
+["24 JUN 24","Ranks 6 24 24.htm"],
+["1 JUL 24","Ranks 7 1 24.htm"],
+["15 JUL 24","Ranks 7 15 24.htm"],
+["22 JUL 24","Ranks 7 22 24.htm"],
+["29 JUL 24","Ranks 7 29 24.htm"],
+["5 AUG 24","Ranks 8 5 24.htm"],
+["12 AUG 24","Ranks 8 12 24.htm"],
+["19 AUG 24","Ranks 8 19 24.htm"],
+["2 SEP 24","Ranks 9 2 24.htm"]
+
+]
+
